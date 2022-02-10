7abce9efadd7fc1d52aeba8fad5a905143ab23fc
SaiReventh <79400697+25Sai@users.noreply.github.com>
2022-02-10 22:33:16 +0530

    Add files via upload

---
+++ pasgrand_package/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+ name='spwge', 
+ version='0.1',
+ author="SaiReventh",
+ author_email="saireventhsr@gmail.com",
+ description="This is a python package to generate random passwords",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
